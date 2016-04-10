ee4803a018a40044b039b6a24a7a9e6cc2a9a413
glasses4days <cooper@Coopers-MacBook-Air.local>
2016-04-09 17:10:49 -0700

    Completed and tested through function 9

--- functions.py
+++ functions.py
@@ -10,15 +10,11 @@ def print_hello_world():
     """Prints 'Hello World'"""
     print "Hello World"
 
-print_hello_world()
-
 # 2. Write a function that takes a name as a string and
 #    prints "Hi" followed by the name.
 def print_user_name(name):
     """Prints name"""
     print "Hi", name
-
-print_user_name('Taurus')
 
 # 3. Write a function that takes two integers and multiplies
 #    them together. Print the result.
@@ -26,35 +22,56 @@ def multiply_integers(x, y):
     """Multiplies x and y then prints the result."""
     print x * y
 
-multiply_integers(5, 6)
 # 4. Write a function that takes a string and an integer and
 #    prints the string that many times
 def print_multiple_times(str, int):
     """Prints given string number of times indicated in argument"""
     print str * int
 
-print_multiple_times("SatanSeitanSanta", 5)
-
 # 5. Write a function that takes an integer and prints "Higher
 #    than 0" if higher than zero and "Lower than 0" if lower
 #    than zero. If integer is 0 print "Zero".
-
+def print_higher_or_lower(num):
+    """Checks if higher than,lower than, or equal to 0 and prints result"""
+    if num > 0:
+        print "Higher than 0"
+    elif num < 0:
+        print "Lower than 0"
+    elif num == 0:
+        print "Zero"
 
 # 6. Write a function that takes an integer and returns a
 #    boolean (True or False), depending on whether the number
 #    is evenly divisible by 3.
-
+def print_divisible_by_three(num):
+    """"Checks if num is evenly divisible by 3 and prints True or False"""
+    if num%3 == 0:
+        print True
+    else:
+        print False
 
 # 7. Write a function that takes a sentence as one string and
 #    returns the number of spaces.
+def count_spaces(sentence):
+    """Checks sentence for number of spaces and returns result."""
+    counter = 0
 
+    for char in sentence:
+        if char == " " :
+            counter += 1
+
+    return counter
 
 # 8. Write a function that can be passed a meal price and a
 #    tip percentage. It should return the total amount paid
 #    (price + price * tip). **However:** passing in the tip
 #    percentage should be optional; if not given, it should
 #    default to 15%.
-
+def calculate_total_meal_cost(price, tip=15):
+    """Calculates meal price with tip. Tip defaults to 15 percent if none given"""
+    meal_total = "{0:.2f}".format(price + price * tip/100)
+    
+    return meal_total
 
 # 9. Write a function that takes an integer as an argument and
 #    returns two pieces of information as strings ---
@@ -66,6 +83,28 @@ print_multiple_times("SatanSeitanSanta", 5)
 # 	variables --- sign and parity (whether it's positive
 # 	or negative). Print sign and parity.
 
+def print_sign_parity(num):
+    """Checks if num is even or odd and positive or negative and prints result."""
+    num_attributes = []
+    
+    if num % 2 == 0:
+        num_attributes.append("Even")
+    else:
+        num_attributes.append("Odd")
+    if num >= 0:
+        num_attributes.append("Positive")
+    else:
+        num_attributes.append("Negative")
+    #unpacks num_attributes and assigns to sign and parity
+    sign, parity = num_attributes
+
+    print sign, parity
+
+print_sign_parity(7)
+print_sign_parity(-5)
+print_sign_parity(4)
+print_sign_parity(-4)
+print_sign_parity(0)
 
 ################################################################
 # PART TWO
